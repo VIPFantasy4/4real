7deb9616643e4db272f2422b1354eb0bbac32e6c
Fan <VIP.Fan.Deng@gmail.com>
2020-07-08 22:04:54 +0800

    gang phase

--- duel/duelcore/phase.py
+++ duel/duelcore/phase.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
 from .exceptions import *
 import log
 import asyncio
@@ -12,25 +13,22 @@ class Phase:
     def __init__(self, chain):
         self._chain = weakref.proxy(chain)
         self._fut = None
-        self._next_phase = None
+        self._next = None
         self._turn = None
-        self._next_turn = None
 
     @property
     def turn(self):
         return self._turn
 
-    def __next__(self):
-        raise NotImplementedError
+    @property
+    def fut(self):
+        return self._fut
 
     def __enter__(self):
         raise NotImplementedError
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._chain.shift(self._next_phase)
-
-    async def tick(self):
-        raise NotImplementedError
+        self._chain.shift(self._next)
 
 
 class DrawPhase(Phase):
@@ -51,13 +49,34 @@ class DrawPhase(Phase):
                         triple[i].setdefault(card[0], []).append(card[1])
             for addr, gambler in random.sample(gamblers.items(), 3):
                 gambler.deal(triple.pop())
-            self._first = self._turn = addr
             key_list = list(gamblers.keys())
-            self._next_turn = key_list[(key_list.index(self.turn) + 1) % 3]
-            self._three = self.DECK[-3:]
+            order = key_list.index(addr)
+            key_list = key_list[order:] + key_list[:order]
+            od = OrderedDict()
+            for key in key_list:
+                od[key] = gamblers[key]
+            self._next = GangPhase(self._chain, self.DECK[-3:], od, addr)
         else:
             log.error('Invalid room%s can not enter DrawPhase', self._chain.duel.view())
             raise DrawPhaseRuntimeError(generate_traceback())
+        return self.till_i_die()
+
+    async def till_i_die(self):
+        await self._chain.duel.send({
+            'name': '',
+            'args': None,
+            'kwargs': None
+        })
+
+
+class GangPhase(Phase):
+    def __init__(self, chain, three, od, turn):
+        super().__init__(chain)
+        self._three = three
+        self._turn = turn
+        self._od = od
+
+    def __enter__(self):
         return self.till_i_die()
 
     async def run_out(self, fut):
@@ -66,11 +85,6 @@ class DrawPhase(Phase):
             fut.set_result(None)
 
     async def till_i_die(self):
-        await self._chain.duel.send({
-            'name': '',
-            'args': None,
-            'kwargs': None
-        })
         fut = asyncio.get_event_loop().create_future()
         self._fut = fut
         task = asyncio.create_task(self.run_out(fut))
@@ -78,29 +92,5 @@ class DrawPhase(Phase):
         if isinstance(choice, int):
             task.cancel()
         self._fut = None
-        _id, status, gamblers = self._chain.duel.view()
         if choice:
-            gamblers[self.turn].role += 1
-        _sum = sum(gambler.role for gambler in gamblers.values())
-        if self._next_turn:
-            key_list = list(gamblers.keys())
-            next_turn = key_list[(key_list.index(self.turn) + 1) % 3]
-            if next_turn is not self._first:
-                self._turn = self._next_turn
-                self._next_turn = key_list[(key_list.index(self._next_turn) + 1) % 3]
-            else:
-                self._next_turn = None
-                if _sum:
-                    if gamblers[self._first].role:
-                        self._turn = self._first
-                    else:
-                        next_turn = key_list[(key_list.index(self._first) + 1) % 3]
-                        if _sum > 2:
-                            self._turn = next_turn
-                        elif gamblers[next_turn].role:
-                            next_turn
-                        else:
-                            self.turn
-                else:
-                    pass
-        else:
+            pass
