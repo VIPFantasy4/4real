5defb5407d605ee78de820bc72f434c5044e73f3
Fan <VIP.Fan.Deng@gmail.com>
2020-09-13 19:48:23 +0800

    hustle

--- mc/drip/developer_mods/srv/4real/srv.py
+++ mc/drip/developer_mods/srv/4real/srv.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from Queue import Queue, Empty
+import apolloCommon.workerPool as workerPool
 import lobbyGame.netgameApi as onlineApi
 import server.extraServerApi as serverApi
+import pickle
+import kafka
 import cfg
+
+KAFKA_TOPICS = [
+    'cli',
+]
+KAFKA_SERVERS = [
+    '122.51.140.131:9092',
+]
 
 
 class Srv(serverApi.GetServerSystemCls()):
@@ -30,16 +41,48 @@ class Srv(serverApi.GetServerSystemCls()):
         self.ListenForEvent(cfg.MOD_NAMESPACE, cfg.MOD_CLI_NAME, 'G_MATCH', self, self.match)
         self.ListenForEvent(cfg.MOD_NAMESPACE, cfg.MOD_CLI_NAME, 'G_COURT', self, self.catch_up)
 
-        self._flag = 1
+        self._q = Queue()
+        self._alive = True
+        self._consumer = kafka.KafkaConsumer(
+            'duel',
+            bootstrap_servers=KAFKA_SERVERS,
+            consumer_timeout_ms=1000,
+            value_deserializer=pickle.loads
+        )
 
     def _update(self):
         pass
 
     def _destroy(self):
-        pass
+        self._alive = False
 
     def serve_forever(self, *args):
-        pass
+        pool = workerPool.ForkNewPool(4)
+        pool.EmitOrder(self.consume_forever.__name__, self.consume_forever, lambda *args: None)
+
+    def consume_forever(self):
+        consumer = self._consumer
+        while self._alive:
+            try:
+                msg = next(consumer)
+            except StopIteration:
+                continue
+            self._q.put(msg.value)
+
+    def process_forever(self):
+        if self._alive:
+            try:
+                data = self._q.get_nowait()
+                duel = None
+                for args in data[2]:
+                    addr = args[0]
+                    pid = onlineApi.GetPlayerIdByUid(addr)
+                    if pid:
+                        if duel is None:
+                            duel = Duel(*data)
+                        self.NotifyToClient(pid, 'G_COURT', {'args': []})
+            except Empty:
+                pass
 
     def connection_made(self, data):
         uid = onlineApi.GetPlayerUid(data.get('id'))
