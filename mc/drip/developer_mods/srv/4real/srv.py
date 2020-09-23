2cafa6aee867d7661d633bbc966dafb33ed9a650
邓瑞贤 <wb.dengruixian@mesg.corp.netease.com>
2020-09-23 17:57:31 +0800

    group id

--- mc/drip/developer_mods/srv/4real/srv.py
+++ mc/drip/developer_mods/srv/4real/srv.py
@@ -99,6 +99,7 @@ class Srv(serverApi.GetServerSystemCls()):
         self._consumer = kafka.KafkaConsumer(
             'duel',
             bootstrap_servers=KAFKA_SERVERS,
+            group_id=str(onlineApi.GetServerId()),
             value_deserializer=pickle.loads
         )
         self._producer = kafka.KafkaProducer(bootstrap_servers=KAFKA_SERVERS, value_serializer=pickle.dumps)
